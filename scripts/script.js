170af3128877dab65436ba7dbfc9f5cec548030d
KadotaTerry <59702894+KadotaTerry@users.noreply.github.com>
2020-05-03 17:14:26 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,30 @@
-var xmlhttp = new XMLHttpRequest();
-var url = "https://github.com/lmowens/FinalProject/blob/master/FinalProjectData.json";
+// NOTE: If you run this file locally
+// You will not get a server status
+// You can comment out lines 9 and 26 to make it work locally
 
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        myFunction(myArr);
+var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
+
+xhr.onload = function() {                       // When readystate changes
+  // The following conditional check will not work locally - only on a server
+  //if(xhr.status === 200) {                      // If server status was ok
+    responseObject = JSON.parse(xhr.responseText);
+
+    // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
+    var newLine = '';
+    for (var i = 0; i < responseObject.syllabusData.length; i++) { // Loop through object
+      responseObject.syllabusData[i].info + '" />';
+      newLine += '<p><b>' + responseObject.syllabusData[i].header1 + '</b><br>';
+      newLine += respnseObject.syllabusData[i].
+      newLine += '<p><b>'+ responseObject.syllabusData[i].info + '</b><br>';
+      newLine += responseObject.syllabusData[i].info + '</p>';
+      newLine += '</div>';
     }
+
+    // Update the page with the new content
+    document.getElementById('Content_area').innerHTML = newContent;
+
+  //}
 };
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
 
-function myFunction(arr) {
-    var out = "";
-    var i;
-    for(i = 0; i < arr.length; i++) {
-        out += '<a href="' + arr[i].url + '">' +
-        arr[i].display + '</a><br>';
-    }
-    document.getElementById("text").innerHTML = out;
-}
+xhr.open('GET' , 'https://kadotaterry.github.io/WebDevFinal/syllabus.json', true);
+xhr.send(null);
